b06abb80a516e22ae33449ea7c6442f348ee5007
Ian Rappaport <ianrappaport@Ians-MacBook-Pro-2.local>
2020-01-16 10:28:00 -0500

    style: updated dashboard home page

--- client/src/components/TableExample.jsx
+++ client/src/components/TableExample.jsx
@@ -23,8 +23,8 @@ class TableExample extends Component {
           class="col-md-9 ml-sm-auto col-lg-10 px-4">
 
           <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 class="h2">Dashboard</h1>
-            <div class="btn-toolbar mb-2 mb-md-0">
+            <h1 class="h2">Profile Page</h1>
+            {/* <div class="btn-toolbar mb-2 mb-md-0">
               <div class="btn-group mr-2">
                 <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
                 <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
@@ -33,10 +33,13 @@ class TableExample extends Component {
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
                 This week
               </button>
-            </div>
+            </div> */}
           </div>
-          <h2>Section title</h2>
-          <div class="table-responsive">
+          {/* <h2>Section title</h2> */}
+          <div>
+            Use Links on Left to update your properties, add new ones, or change your profile
+          </div>
+          {/* <div class="table-responsive">
           <Table striped bordered hover variant="dark">
               <thead>
                 <tr>
@@ -162,7 +165,7 @@ class TableExample extends Component {
                 </tr>
               </tbody>
             </Table>
-          </div>
+          </div> */}
         </main>
 
 
